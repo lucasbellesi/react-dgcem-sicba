5ed73cc0ef779ba7a678e69b675e3fe41cf2009d
ngrosso <nzgrosso@gmail.com>
2018-05-04 12:04:33 -0300

    rollback

--- src/App.js
+++ src/App.js
@@ -47,10 +47,20 @@ const App = (props, state) => (
     <MainView>
       <Box>
 <<<<<<< HEAD
+<<<<<<< HEAD
         lalalan fue una pelicula
 =======
         rararan 
 >>>>>>> 72d85bcf12a4fc9073eb15f6fdd296d77a31fbc8
+=======
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+>>>>>>> parent of 72d85bcf... Update App.js
       </Box>
       <Box>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
